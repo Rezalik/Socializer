6375e09939341d1a12d458149651920d6d5db002
Alexey Ivanov <aleksey.ivanov1@gmail.com>
2014-04-09 13:34:29 +0400

    Initial Commit

---
+++ Socializer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Socializer
+//
+//  Created by Alexey Ivanov on 09.04.14.
+//  Copyright (c) 2014 Alexey Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Socializer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Socializer
+//
+//  Created by Alexey Ivanov on 09.04.14.
+//  Copyright (c) 2014 Alexey Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
